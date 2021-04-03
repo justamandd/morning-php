9947acb8397b749aeb26041ad35346e11db75e5c
Justamandd <justamandd@gmail.com>
2021-04-03 14:39:47 -0300

    arquivos de validacao do cadastro de usuario funcionando

--- view/reg/user.php
+++ view/reg/user.php
@@ -7,12 +7,26 @@
             <input type="email" name="email" id="email" class="form-control mt-3" placeholder="Email" required value="<?php echo isset($user)?$user->getEmail():'' ?>">
             <input type="date" name="dtBirthday" id="dtBirthday" class="form-control mt-3" required value="<?php echo isset($user)?$user->getDtBirth():'' ?>">
             <input type="text" name="user" id="user" class="form-control mt-3" placeholder="Username" required value="<?php echo isset($user)?$user->getUser():'' ?>">
-            <input type="password" name="password" id="password" class="form-control mt-3" placeholder="Password" required>
+            <input type="password" name="password" id="password" class="form-control mt-3" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
             <input type="hidden" name="type" id="type" value="0">
-            <button type="submit" name="confirmBtn" id="confirmBtn" class="btn btn-success mt-3">Sign Up</button>
+            <div class="mt-2">
+                <input type="checkbox" onclick="showPassword()" id="cbSPw">
+                <label for="cbSPw">Show password</label>
+            </div>
+            <div class="alert mt-2" id="alert" style="display: none">
+                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                <h3>Password must contain the following:</h3>
+                <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
+                <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
+                <p id="number" class="invalid">A <b>number</b></p>
+                <p id="length" class="invalid">Minimum <b>8 characters</b></p>
+            </div>
+            <button type="submit" name="confirmBtn" id="confirmBtn" class="btn btn-success mt-2">Sign Up</button>
         </div>
     </div>
 </form>
+<script src="./public/scripts/validation.js">
+</script>
 
 <?php
 
